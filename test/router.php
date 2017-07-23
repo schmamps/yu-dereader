05fdb62ab8e1fdb1e2239ea4c7b396011620c8cd
Andrew Champion <awchampion@gmail.com>
2017-07-23 12:35:51 -0700

    janky test updates

--- test/router.php
+++ test/router.php
@@ -5,9 +5,44 @@ $doc = ($url === '/') ? '/404' : $url;
 $path = $base . $doc;
 $ext =  array_slice(explode('.', $path), -1)[0];
 
+
+
+function gray() {
+    return[
+        'iVBORw0KGgoAAAANSUhEUgAAACAAAABACAQAAADRh70lAAAAK0lEQVR42u3MMREAAAgE',
+        'ID+50TWEmwcBSE+dRCAQCAQCgUAgEAgEAsH3YAG/G1/BhhLG4gAAAABJRU5ErkJggg=='
+    ];
+}
+
+function red() {
+    return [
+        'iVBORw0KGgoAAAANSUhEUgAAAEAAAAAQCAYAAACm53kpAAAALElEQVR42u3QAQEAAAgC',
+        'oFru9BoiTGAvuSm2AgQIECBAgAABAgQIECBAQJ8HrvEv0TxIF5QAAAAASUVORK5CYII='
+    ];
+}
+
+function png($url) {
+    header('Content-Type: image/png');
+
+    $content = (strstr($url, 'lastever.png')) ? red() : gray();
+
+    print base64_decode(implode('', $content));
+}
+
+header(sprintf(
+    'X-Whatver: %s, %s, %s',
+    var_export($url, TRUE),
+    var_export($path, TRUE),
+    var_export($url === '/lastever.png', TRUE)
+));
+
+
 if (file_exists($path)) {
     header('Content-Type: text/' . $ext);
     include($path);
+}
+else if (substr($url, -4) === '.png') {
+    png($url);
 }
 else {
     header('Status: 404');
