5ee45145500187ef1598809ecd0c9fb0dc3a7aa7
Andrew Champion <awchampion@gmail.com>
2020-09-15 19:11:14 -0700

    remove refactored sources

--- src/js/core.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const core = {  // eslint-disable-line no-unused-vars
-	BIWRBR: 'butiwouldratherbereading',
-
-	compose: (...funcs) => (value = null) => (
-		funcs.reduce((val, fn) => fn(val), value)
-	)
-};
