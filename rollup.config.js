fc58b65ecbd81da4fe9e04d7d4142353f281cbf5
Andrew Champion <awchampion@gmail.com>
2020-09-15 19:45:53 -0700

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,10 @@
 import typescript from '@rollup/plugin-typescript';
 import jsonImport from '@rollup/plugin-json';
-// import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
 
 export default {
 	input: 'src/ts/index.ts',
 	output: {
-		// dir: 'build',
 		file: 'build.safariextension/yu.js',
 		format: 'iife',
 	},
@@ -19,5 +17,4 @@ export default {
 		jsonImport(),
 		terser(),
 	],
-	// external: ['tslib'],
 };
