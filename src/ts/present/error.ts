98efc560c89d1d793cd5578ec55fbd21f119b419
Andrew Champion <awchampion@gmail.com>
2021-04-09 14:29:45 -0700

    remove present module

--- src/ts/present/error.ts
+++
@@ -1,5 +0,0 @@
-export const error = (desc) => async function error(e) {
-	console.group(desc);
-	console.error(e);
-	console.groupEnd();
-};
--- src/ts/present/index.ts
+++
@@ -1,4 +0,0 @@
-export { list as eggs } from './eggs';
-export { error } from './error';
-export { list as views } from './views';
-export { init as interface } from './interface';
