45cd9aa771fdd5ea44b175c1c2ebd3bb53c1ace4
Andrew Champion <awchampion@gmail.com>
2021-05-07 15:27:27 -0700

    update for rendering default change

--- src/ts/consts.ts
+++ src/ts/consts.ts
@@ -2,4 +2,4 @@ export const NOT_FOUND = '(not found)';
 export const RATHER = 'butiwouldratherbereading';
 export const TRANS_DURATION = 800;
 export const ID = 'comic';
-export const PIXELATED = 'pixel';
+export const UNPIXEL = 'unpixelate';
