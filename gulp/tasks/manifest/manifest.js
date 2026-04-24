1a7dcdba19bda786a84f13a30526842c8acba7bf
Andrew Champion <awchampion@gmail.com>
2026-04-24 15:06:11 -0700

    remove unused icons

--- gulp/tasks/manifest/manifest.js
+++ gulp/tasks/manifest/manifest.js
@@ -5,9 +5,9 @@ const log = require('../../lib/log');
 const path = require('../../lib/path');
 const meta = require('../../lib/task/meta');
 
-const DEFAULTS = {server: {host: 'localhost', port: 4242,},};
+const DEFAULTS = { server: { host: 'localhost', port: 4242, }, };
 
-const abstract = meta.abstract({in: 'manifest.json', sub: 'meta',});
+const abstract = meta.abstract({ in: 'manifest.json', sub: 'meta', });
 const desc = meta.describe('update the Chrome extension manifest');
 
 const err = (err) => {
@@ -21,7 +21,7 @@ const reduceEnv = (env, kv) => {
 		return env;
 	}
 
-	return Object.assign(env, {[keySpec.substr(-4)]: val});
+	return Object.assign(env, { [keySpec.substr(-4)]: val });
 };
 
 const parseEnv = (content) => {
@@ -31,7 +31,7 @@ const parseEnv = (content) => {
 		map((line) => line.split('=')).
 		map((pair) => pair.map((item) => item.toLowerCase().trim())).
 		reduce(reduceEnv, {})
-	;
+		;
 
 	return Object.assign({}, DEFAULTS.server, env);
 };
@@ -64,36 +64,26 @@ const getBuildSettings = (pkg, manif, prod) => {
 		concat(pkg.version.split('.')).
 		slice(0, 3).
 		join('.');
-	const {host, port} = getTestServer();
+	const { host, port } = getTestServer();
 	for (const proto of ['http', 'https']) {
 		manif.content_scripts[0].matches.push(`${proto}://${host}:${port}/*`);
 	}
 
 	// eslint-disable-next-line camelcase
-	return {version, content_scripts: manif.content_scripts};
+	return { version, content_scripts: manif.content_scripts };
 };
 
-const getIcons = (manif, prod) => {
-	if (!prod) {
-		return manif.icons;
-	}
-
-	return {
-		'128': manif.web_accessible_resources[0].resources[0],
-	};
-};
 
 const update = (dataSources) => {
 	const [manif, pkg, prod] = dataSources;
 	const name = pkg.description;
-	const {version, content_scripts: cs} = getBuildSettings(pkg, manif, prod);
-	const icons = getIcons(manif, prod);
+	const { version, content_scripts: cs } = getBuildSettings(pkg, manif, prod);
 
 	return Object.assign(
 		{},
 		manif,
 		// eslint-disable-next-line camelcase
-		{name, version, content_scripts: cs, icons});
+		{ name, version, content_scripts: cs });
 };
 
 const write = (data, cfg) => {
@@ -103,9 +93,9 @@ const write = (data, cfg) => {
 
 	return fs.
 		promises.
-		mkdir(cfg.dir, {recursive: true}).
+		mkdir(cfg.dir, { recursive: true }).
 		then(writeToDisk)
-	;
+		;
 };
 
 const run = () => {
@@ -121,7 +111,7 @@ const run = () => {
 		then(update).
 		then(writeManifest).
 		catch(err)
-	;
+		;
 };
 
 module.exports = {
