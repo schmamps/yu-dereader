246524ea55e16060eb74e65900c1493260fa745c
Andrew Champion <awchampion@gmail.com>
2026-05-16 15:36:51 -0700

    edit for clarity

--- gulp/help.js
+++ gulp/help.js
@@ -11,15 +11,17 @@ const getEntry = (pad) => (task) => {
 const listEntries = async (tasks) => {
 	const keys = Object.keys(tasks);
 	const pad = keys.reduce((max, key) => Math.max(max, key.length), 0);
+	const describe = (name) => {
+		const desc = tasks[name].desc();
 
-	return keys.
-		map((name) => ({ name, desc: tasks[name].desc() })).
+		return { name, desc };
+	}
+
+	return keys.map((name) => ({ name, desc: tasks[name].desc() })).
 		map(getEntry(pad));
 };
 
 const getHeader = (ents) => {
-
-
 	const pad = ents.reduce((max, line) => Math.max(max, line.length), 0);
 	const title = [
 		'Task Menu',
