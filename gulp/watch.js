9f32b69483625128d8408417ddb683c8bb76ae44
Andrew Champion <awchampion@gmail.com>
2026-05-16 15:36:26 -0700

    (re-)implement watch task

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,10 +1,25 @@
+import * as meta from './metadata/index.js';
+import * as gulp from 'gulp';
+
+
 const init = (explicit) => {
+	const WATCH = Object.keys(explicit).filter((key) => key != 'manifest');
+
 	return {
-		run: async () => { },
-		desc: async () => { return 'watch'; },
-		abstract: {}
+		abstract: {},
+		desc: meta.describe(
+			`watch for updated files in tasks [${WATCH.join(', ')}]`
+		),
+		run: async () => {
+			WATCH.forEach((taskName) => {
+				const abstract = explicit[taskName].abstract;
+				const run = explicit[taskName].run;
+
+				gulp.watch(abstract.src, () => run());
+			});
+		},
 	};
-}
+};
 
 export {
 	init,
