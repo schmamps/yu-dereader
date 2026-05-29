4adbbf3731e2ca91dd1e98fe42ca6cbe43d217c7
Andrew Champion <awchampion@gmail.com>
2026-05-29 13:33:18 -0700

    add bundle as explicit task

--- gulp/index.js
+++ gulp/index.js
@@ -1,13 +1,14 @@
 import { task as css } from './css.js';
 import { task as js } from './js.js';
 import { task as manifest } from './manifest.js';
+import { task as bundle } from './bundle.js';
 import { init as watchInit } from './watch.js';
 import { init as buildInit } from './build.js';
 import { init as helpInit } from './help.js';
 
 
 const listAllTasks = () => {
-	const explicit = { css, js, manifest, };
+	const explicit = { css, js, manifest, bundle, };
 	const watch = watchInit(explicit);
 	const build = buildInit(explicit);
 	const help = helpInit(Object.assign({}, explicit, { watch, build, }));
