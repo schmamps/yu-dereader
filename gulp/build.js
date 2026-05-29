c7f704dafe03e9b80f4d1470f4dd0267d4eb3f09
Andrew Champion <awchampion@gmail.com>
2026-05-29 13:33:02 -0700

    add bundler to chain

--- gulp/build.js
+++ gulp/build.js
@@ -4,9 +4,13 @@ const init = (explicit) => {
 	const abstract = {};
 	const description = meta.describe('run all tasks');
 	const run = async () => {
-		return await Promise.all([
-			Object.keys(explicit).map((taskName) => explicit[taskName]()),
-		]);
+		await Promise.all(Object.
+			keys(explicit).
+			filter((key) => key !== 'bundle').
+			map((taskName) => explicit[taskName]())
+		);
+
+		return await explicit.bundle();
 	};
 
 	const task = Object.assign(run, { abstract, description, });
