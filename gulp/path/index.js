1a929e8dcd1954a53696b7db43a5a30f9fc8e6a3
Andrew Champion <awchampion@gmail.com>
2026-06-08 16:19:12 -0700

    extends builtin path library

--- gulp/path/index.js
+++ gulp/path/index.js
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import * as path from 'path';
+import { join, resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const isProjectDir = (path) => {
@@ -7,7 +7,7 @@ const isProjectDir = (path) => {
 }
 
 const getProjectDir = () => {
-	let pd = path.dirname(fileURLToPath(import.meta.url));
+	let pd = dirname(fileURLToPath(import.meta.url));
 
 	while (!isProjectDir(pd)) {
 		pd = fs.realpathSync([pd, '..'].join('/'));
@@ -23,24 +23,11 @@ const joinDirs = (...args) => {
 		replace(/\/+/g, '/');
 }
 
-const split = (path) => path.split(/[\\\/]+/g);
-const basename = (path) => split(path).pop();
-const parent = (path) => split(path).slice(0, -1).join('/');
-const resolve = (...args) => {
-	console.group('resolve');
-	console.dir(args);
-	console.groupEnd();
-
-	return 1 / 0;
-}
 const ROOT = getProjectDir();
 
 
 export {
-	split,
-	basename,
-	parent,
-	joinDirs as join,
-	resolve,
 	ROOT,
+	join,
+	resolve,
 };
